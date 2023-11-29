fde0c55098f9c62073c1196872c494b60ad6904f
Xi Lin <xlin32@ncsu.edu>
2023-11-29 15:27:27 -0500

    Modeling into subtab finished

--- server.R
+++ server.R
@@ -3,38 +3,59 @@ library(tidyverse)
 
 adult <- read.table('https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data', 
                     sep = ',', fill = F, strip.white = T)
-colnames(adult) <- c('age', 'workclass', 'fnlwgt', 'educatoin', 
-                     'educatoin_num', 'marital_status', 'occupation', 'relationship', 'race', 'sex', 
+colnames(adult) <- c('age', 'workclass', 'fnlwgt', 'education', 
+                     'education_num', 'marital_status', 'occupation', 'relationship', 'race', 'sex', 
                      'capital_gain', 'capital_loss', 'hours_per_week', 'native_country', 'income')
 
 adult[adult == "?"] <- "Unknown"
 
 adult <- adult %>% mutate(Native = if_else(native_country=="United-States","Y","N"))
 
-adult <- adult %>% select(-fnlwgt,-educatoin,-relationship, -native_country)
+adult <- adult %>% select(-fnlwgt,-education,-relationship, -native_country)
 
 shinyServer(function(input, output, session) {
   
+
+# Data exploration --------------------------------------------------------
+
   getData <- reactive({
-    newData <- adult %>% filter(sex == input$sex)
+    newData <- adult %>% filter(income == input$hist_cat_income)
   })
   
   output$info <- renderText({
     #get filtered data
     newData <- getData()
-    paste("The average education years of", input$sex, "is", round(mean(newData$education_num, na.rm = TRUE), 1), "and the average hours per week is", round(mean(newData$hours_per_week, na.rm = TRUE), 1), sep = " ")
+    paste("The average education years of ", input$hist_cat_income, " income level is", round(mean(newData$education_num, na.rm = TRUE), 1), "and the average hours per week is", round(mean(newData$hours_per_week, na.rm = TRUE), 1), sep = " ")
   })
   
+  # histogram
   output$hist <- renderPlot({
     newData <- getData()
-    g <- ggplot(newData, aes(x = as.numeric(input$numchoice), group=income, fill=income))
+    g <- ggplot(newData, aes(x = as.numeric(input$hist_num_var), group=input$hist_cat_var, fill=input$hist_cat_var))
     g + geom_histogram(binwidth=1, color='black')
   })
   
+  # bar plot
   output$bar <- renderPlot({
-    newData <- getData()
-    g <- ggplot(newData, aes(x = as.numeric(input$catchoice), group=income, fill=income))
-    g + geom_bar(stat = "identity")
+    g <- ggplot(adult, aes(x = input$bar_cat_var1, group=input$bar_cat_var_2, fill=input$bar_cat_var_2))
+    g + geom_bar()
   })
+  
+  # scatter plot
+  output$bar <- renderPlot({
+    g <- ggplot(adult, aes(x = as.numeric(input$scatter_num_var_1), y = as.numeric(input$scatter_num_var_2), group=input$scatter_cat_var))
+    g + point()
+  })
+  
+  # box plot
+  output$bar <- renderPlot({
+    g <- ggplot(adult, aes(x = as.factor(input$box_cat_var), y = as.numeric(input$box_num_var)))
+    g + boxplot()
+  })
+  
+
+# Modeling ----------------------------------------------------------------
+
+  
   
 })
--- ui.R
+++ ui.R
@@ -15,8 +15,14 @@ fluidPage(
   # Application title
   titlePanel("Old Faithful Geyser Data"),
   
+  # MathJax
+  withMathJax(),
+  
+  # Top level tabs
   tabsetPanel(
     type = "tabs",
+    
+    # About Tab
     tabPanel(
       "About",
       tags$br(),
@@ -108,36 +114,41 @@ fluidPage(
           tags$td("Binary"),
           tags$td("Income"),
           tags$td(">50K, <=50K."))
-        ), #closing table
+        ), 
+      #closing table
       tags$br(),
       tags$p("Since fnlwgt is not relevent to this study, while education and relationship have similar infomation with education_num and marital_status, I removed these three variables from the dataset."),
       tags$p("Moreover, I created a variable 'Native' indicating whether the worder's native country was United States or not to replace the native_country variable.")
-    ), #closing tab panel
+    ), 
+    #closing tab panel
     
+    # Data Exploration Tab
     tabPanel(
       "Data Exploration",
       sidebarLayout(
         sidebarPanel(
+          
+          # Four type of plots
           selectInput("plottype",
-                      "Choose your plot", # Four type of plots
+                      "Choose your plot", 
                       c("Histogram" = "hist",
                         "Bar Graph" = "bar",
                         "Scatter Plot" = "scatter",
                         "Box Plot" = "box")
             ),
           
-          conditionalPanel( # histogram - numerical var
+          # histogram - numerical var
+          conditionalPanel( 
             condition = "input.plottype == 'hist'",
             selectInput("hist_num_var",
                         "Choose the numerical variable that you want to investigate:",
                         c("Age" = "age",
                           "Years of education" = "education_num",
-                          "Working hours per week" = "hours_per_week",
-                          "Capital gain" = "capital_gain",
-                          "Capital loss" = "capital_loss"))
+                          "Working hours per week" = "hours_per_week"))
           ),
           
-          conditionalPanel( # histogram - categorical var comparison
+          # histogram - categorical var comparison
+          conditionalPanel( 
             condition =  "input.plottype == 'hist'",
             selectInput("hist_cat_var",
                         "Choose the categorical variable that you want to compare:",
@@ -149,7 +160,8 @@ fluidPage(
                           "Native or International" =  "Native"))
           ),
           
-          conditionalPanel( # histogram - row filter on income
+          # histogram - row filter on income
+          conditionalPanel( 
             condition =  "input.plottype == 'hist'",
             selectInput("hist_cat_income",
                         "filter the income level",
@@ -157,9 +169,10 @@ fluidPage(
                           ">50K" = ">50K"))
           ),
           
-          conditionalPanel( # bar plot - categorical var
+          # bar plot - categorical var
+          conditionalPanel( 
             condition =  "input.plottype == 'bar'",
-            selectInput("bar_cat_var",
+            selectInput("bar_cat_var_1",
                         "Choose the categorical variable that you want to investigate:",
                         c("Work class" = "workclass",
                           "Marital status" = "marital_status",
@@ -170,31 +183,44 @@ fluidPage(
                           "Income Category" = "income"))
           ),
           
-          conditionalPanel( # scatter plot - numerical & numerical
+          conditionalPanel(
+            condition =  "input.plottype == 'bar'",
+            selectInput("bar_cat_var_2",
+                        "Choose the categorical variable that you want to compare:",
+                        c("Work class" = "workclass",
+                          "Marital status" = "marital_status",
+                          "Occupation" = "occupation",
+                          "Race" = "race",
+                          "Gender" = "sex",
+                          "Native or International" =  "Native",
+                          "Income Category" = "income"),
+                        selected = "marital_status")
+          ),
+          
+          # scatter plot - numerical & numerical
+          conditionalPanel(
             condition = "input.plottype == 'scatter'",
             selectInput("scatter_num_var_1",
                         "Choose the first numerical variable that you want to investigate:",
                         c("Age" = "age",
                           "Years of education" = "education_num",
-                          "Working hours per week" = "hours_per_week",
-                          "Capital gain" = "capital_gain",
-                          "Capital loss" = "capital_loss"),
+                          "Working hours per week" = "hours_per_week"),
                         selected = "age")
           ),
           
-          conditionalPanel( # scatter plot - numerical & numerical
+          # scatter plot - numerical & numerical
+          conditionalPanel(
             condition = "input.plottype == 'scatter'",
             selectInput("scatter_num_var_2",
                         "Choose the second numerical variable that you want to investigate:",
                         c("Age" = "age",
                           "Years of education" = "education_num",
-                          "Working hours per week" = "hours_per_week",
-                          "Capital gain" = "capital_gain",
-                          "Capital loss" = "capital_loss"),
+                          "Working hours per week" = "hours_per_week"),
                         selected = "education_num")
           ),
           
-          conditionalPanel( # scatter plot - numerical & numerical
+          # scatter plot - numerical & numerical
+          conditionalPanel( 
             condition = "input.plottype == 'scatter'",
             selectInput("scatter_cat_var",
                         "Choose the categirical variable that you want to compare:",
@@ -207,18 +233,18 @@ fluidPage(
                           "Income Category" = "income"))
           ),
           
-          conditionalPanel( # box plot - numerical & categorical
+          # box plot - numerical & categorical
+          conditionalPanel(
             condition = "input.plottype == 'box'",
             selectInput("box_num_var",
                         "Choose the numerical variable that you want to investigate:",
                         c("Age" = "age",
                           "Years of education" = "education_num",
-                          "Working hours per week" = "hours_per_week",
-                          "Capital gain" = "capital_gain",
-                          "Capital loss" = "capital_loss"))
+                          "Working hours per week" = "hours_per_week"))
           ),
           
-          conditionalPanel( # box plot - numerical & categorical
+          # box plot - numerical & categorical
+          conditionalPanel( 
             condition = "input.plottype == 'box'",
             selectInput("box_cat_var",
                         "Choose the categorical variable that you want to compare:",
@@ -231,45 +257,136 @@ fluidPage(
                           "Income Category" = "income"))
           ),
           
-          
-          
-        ), #closing data exploration sidebarpanel
+        ), 
+        #closing data exploration sidebarpanel
         
         # Show a plot of the generated distribution
         mainPanel(
           
-        ) # closing data exploration mainpanel
-      ) # closing data exploration sidebarlayout
-  ), # closing data exploration tabpanel
+        ) 
+        # closing data exploration mainpanel
+        
+      ) 
+      # closing data exploration sidebarlayout
+      
+  ), 
+  # closing data exploration tabpanel
   
-  tabPanel(
-    "Modeling",
-    
-    tabsetPanel(
-      tabPanel(
-        "Modeling Info"
-      ),
-      
-      tabPanel(
-        "Model Fitting"
-      ),
-      
-      tabPanel(
-        "Prediction"
+  
+    # Modeling Tab
+    tabPanel(
+      "Modeling",
+      # Sub tabs
+      tabsetPanel(
+        
+        # sub tab 1: Modeling Info
+        tabPanel(
+          "Modeling Info",
+          tags$h3("Logistic Regression"),
+          tags$p("Logistic regression is a statistical method used for binary classification. It is a type of regression analysis that models the probability of a binary outcome (1/0, Yes/No, True/False) as a function of one or more predictor variables. The logistic regression model is named after the logistic function, also known as the sigmoid function, which is used to transform the linear combination of predictor variables into probabilities."),
+          tags$p("Here's a basic overview of how logistic regression works:"),
+          tags$ol(
+            tags$li(tags$b("Model Representation:"),
+                    tags$ul(tags$li("The logistic regression model is represented as follows:",
+                            tags$br(),
+                            '$$P(Y=1)=\\frac{1}{1+e^{-(\\beta_0 + \\beta_1 X_1 + \\beta_2 X_2 + \\dots + \\beta_n X_n)}}$$',
+                            tags$br(),
+                            "where:"),
+                            tags$li(
+                              '\\(P(Y=1)\\) is the probability of the event occurring (the dependent variable being 1),'
+                              ),
+                            tags$li(
+                              '\\(e\\) is the base of the natural logarithm,'
+                            ),
+                            tags$li(
+                              '\\(\\beta_0,\\beta_1,\\dots,\\beta_n\\) are the coefficients,'
+                            ),
+                            tags$li(
+                              '\\(X_1,X_2,\\dots,X_n\\) are the predictor variables.'
+                            )
+                            )
+                    ),        
+            tags$li(tags$b("Sigmoid Function:"),
+                    tags$ul(tags$li(
+                      'The sigmoid function \\((\\frac{1}{1+e^{-z}})\\)  is used to transform the linear combination \\(z\\)  into a value between 0 and 1. This is crucial for interpreting the result as a probability.'
+                    ))),
+            tags$li(tags$b("Training:"),
+                    tags$ul(tags$li(
+                      'The coefficients \\(\\beta\\) are estimated through a process called maximum likelihood estimation. The goal is to find the values of the coefficients that maximize the likelihood of observing the given set of outcomes.'
+                    ))),
+            tags$li(tags$b("Decision Boundary:"),
+                    tags$ul(tags$li(
+                      'The logistic regression model predicts the probability that an instance belongs to a particular class. A decision boundary is set (usually 0.5) such that if the predicted probability is greater than or equal to the decision boundary, the instance is classified as class 1; otherwise, it is classified as class 0.'
+                    )))),
+          tags$p("Logistic regression is widely used in various fields, such as finance, healthcare, and social sciences, for tasks like predicting whether a customer will churn, whether a patient has a particular disease, or whether a student will pass an exam. It's important to note that logistic regression is specifically designed for binary classification problems; for multi-class problems, extensions like multinomial logistic regression are used."),
+          tags$h4("Benefits of Logistic Regression:"),
+          tags$ol(
+            tags$li("Simple to Implement and Interpret"),
+            tags$li("Efficient for Small Datasets"),
+            tags$li("Probabilistic Output"),
+            tags$li("No Assumption of Linearity"),
+            tags$li("Works Well with Feature Scaling"),
+            tags$li("Less Susceptible to Overfitting")
+          ),
+          tags$h4("Drawbacks of Logistic Regression:"),
+          tags$ol(
+            tags$li("Assumption of Linearity"),
+            tags$li("Limited Expressiveness"),
+            tags$li("Not Suitable for Complex Relationships"),
+            tags$li("Sensitive to Outliers"),
+            tags$li("Independence of Errors"),
+            tags$li("Requires a Large Sample Size for Stable Estimates")
+          ),
+          tags$br(),
+          tags$h3("Random Forest"),
+          tags$p("Random Forest is an ensemble learning method used for both classification and regression tasks. It operates by constructing a multitude of decision trees during training and outputs the class that is the mode of the classes (classification) or the mean prediction (regression) of the individual trees."),
+          tags$p(tags$b("Key features of random forest:")),
+          tags$ol(
+            tags$li(tags$b("High accuracy:"),"Random forest is one of the most accurate machine learning algorithms available."),
+            tags$li(tags$b("Robustness to overfitting:"),"Random forest is relatively robust to overfitting, which means that it is less likely to make poor predictions on new data."),
+            tags$li(tags$b("Interpretability:"),"Random forest is relatively interpretable, which means that it is possible to understand how the algorithm is making its predictions.")
+          ),
+          tags$h4("Benefits of Random Forest:"),
+          tags$ol(
+            tags$li("High accuracy"),
+            tags$li("Robust to Overfitting"),
+            tags$li("Handles Non-Linearity and Interactions"),
+            tags$li("Works Well with Large Datasets"),
+            tags$li("Implicit Feature Selection"),
+            tags$li("Deals with Imbalanced Datasets")
+          ),
+          tags$h4("Drawbacks of Random Forest:"),
+          tags$ol(
+            tags$li("Lack of Interpretability"),
+            tags$li("Computational Complexity"),
+            tags$li("Sensitive to Noisy Data"),
+            tags$li("Parameter Tuning Complexity"),
+            tags$li("Not Suitable for Very Sparse Datasets")
+          )
+          
+        ),
+        # sub tab 2: Model Fitting
+        tabPanel(
+          "Model Fitting"
+        ),
+        
+        # sub tab 3: Prediction
+        tabPanel(
+          "Prediction"
+        )
       )
-    )
-    # sidebarLayout(
-    #   sidebarPanel(
-    #     select
-    #     
-    #   ), #closing modeling sidebarpanel
-    #   
-    #   mainPanel(
-    #     
-    #   ) # closing modeling mainpanel
-    # ) # closing modeling sidebarlayout
-  ) # closing modeling tabpanel
-  
+      # sidebarLayout(
+      #   sidebarPanel(
+      #     select
+      #     
+      #   ), #closing modeling sidebarpanel
+      #   
+      #   mainPanel(
+      #     
+      #   ) # closing modeling mainpanel
+      # ) # closing modeling sidebarlayout
+    ) # closing modeling tabpanel
+    
 )#closing tabsetpanel
 
 )  
